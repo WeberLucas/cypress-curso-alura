8d080fc90f0634a984393aaac74d636fcdd2dbd5
wlucas <wlucas@gattecnologia.com.br>
2023-03-29 14:17:23 -0300

    add cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ss5efv",
   e2e: {
     baseUrl: "https://alura-fotos.herokuapp.com",
     apiServer: "https://apialurapic.herokuapp.com",
